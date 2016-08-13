7021628b3b6f9061ce4b920c428aad58507ec0da
rojitaspc <rojitaspc@gmail.com>
2016-08-13 08:59:30 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+	#header('location: public') ;
+	header('location: inicio/') ;
+	#http://localhost/municha/home
+
+// $componentes_url=parse_url($_SERVER["REQUEST_URI"]);
+// $ruta=$componentes_url['path'];
+// $partes_url=explode("/", $ruta);
+
+// echo $partes_url[2].'<p></p>';
+
+// if ($partes_url[2]=="inicio") {
+// 	include_once("public/index.php");
+// }elseif ($partes_url[2]=="municipalidad") {
+// 	include_once("public/chachapoyas.php");
+// }elseif ($partes_url[2]=="concejo") {
+// 	include_once("public/chachapoyas.php");
+// }elseif ($partes_url[2]=="gerencias") {
+// 	include_once("public/gerencias.php");
+// }elseif ($partes_url[2]=="servicios") {
+// 	include_once("public/chachapoyas.php");
+// }elseif ($partes_url[2]=="contactenos") {
+// 	include_once("public/contacto.php");
+// }else{
+// 	echo "error 404";
+// }
+
+?>
+
